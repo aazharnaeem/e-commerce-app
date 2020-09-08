3be501e5059e31398fe4b5590dee67d602b81a83
Azharnaeem <azharnaeem76@gmail.com>
2020-09-08 16:28:09 +0500

    Finalization

--- src/pages/index.js
+++ src/pages/index.js
@@ -25,29 +25,43 @@ const Home = () => {
       </div>
 
     <div className="info">
+
       <h2>WE ARE ELEGANT</h2>
+      
       <p><em> Elegance isn't solely defined bywhat you wear. It's how you carry yourself.</em></p>
+      
       <div className="info-sub">
+      
       <div className="sub-inf">
+      
         <img src={Gift} alt="Icons"></img>
         <h4>BEAUTIFUL PRODUCTS</h4>
         <p>I never look at other people's work. My mind has to be completely focused on my own illusions. It's a philosophy of life. A practice with no end</p>
+      
       </div>
+      
       <div className="sub-inf">
+      
       <img src={Globe} alt="Icons"></img>
 
         <h4>FAST DELIVERY</h4>
         <p>If you do this, something will change, what will change is that you will change, your life will change, and if you can change you, you can perhaps change the world.</p>
+      
       </div>
+      
       <div className="sub-inf">
+      
       <img src={star} alt="Icons"></img>
         <h4>100% SATISFACTION</h4>
         <p>I've always thought of the T-shirt as the Alpha and Omega of the fashion alphabet. I like the things around me to be beautiful and slightly dreamy.</p>
+      
       </div>
       </div>
     </div>
     <hr style={{opacity:0.1}}/>
+
         <Collection/>
+    
         <div  style={{textAlign:"center"}}>
         <Link to="/product"><button id="pro-btn" >
           SEE ALL PRODUCTS
